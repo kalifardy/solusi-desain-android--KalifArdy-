3a43f64cee45b27720eab06d48b9ef0293c8d19c
Kalif ardy noerwicaksono <kalifardy56@gmail.com>
2020-09-18 06:41:19 +0700

    Update String Manipulation.kt

--- sortingkotlin/src/String Manipulation.kt
+++ sortingkotlin/src/String Manipulation.kt
@@ -21,16 +21,4 @@ fun main() {
     print(makanan2.elementAtOrNull(7))
     print(makanan2.elementAt(6))
     print(makanan2.elementAt(5))
-//    val myTasks = setOf("Eat Breakfast", "Clean Up", "Work", "Eat Lunch",  "Clean Up", "Work", "Eat Dinner", "Clean Up")
-//
-//
-//    // Write your code below
-//    for (task in makanan) {
-//        println(task)
-//    }
-//    println("\n-- myTasks Output --")
-//    // Write your code below
-//    for ((taskIndex, task) in myTasks.withIndex()) {
-//        println("$taskIndex: $task")
-//    }
 }
