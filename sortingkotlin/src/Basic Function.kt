493d44e1742ff88aacbe698e46e2059eaf23db29
Kalif ardy noerwicaksono <kalifardy56@gmail.com>
2020-09-18 09:17:56 +0700

    TryOutSekolahDesain

--- sortingkotlin/src/Basic Function.kt
+++ sortingkotlin/src/Basic Function.kt
@@ -1,19 +1,8 @@
-fun hello(name: String): String {
-    val user = "Kalif!"
-    val noString=""
-
-    println("Hello "+ user)
-
-    if(noString=="a"){
-      println(noString) //name berisi string kosong
-    }else{
-        println("Hello "+ name)
-    }
-
-    return "Hello " + name// name tidak memiliki nilai
+fun hello(name: String = "Hello"): String {
+    return "Hello, $name!"
 }
 
-fun main(args: Array<String>) {
-    println(hello("World!"))
+fun main () {
+    println(hello("Kalif ardy"))
+    println(hello())
 }
-
