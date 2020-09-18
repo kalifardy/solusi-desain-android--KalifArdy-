49fe2899223570d54f6d95326647eef2c870399c
Kalif ardy noerwicaksono <kalifardy56@gmail.com>
2020-09-18 09:18:54 +0700

    TryOutSekolahDesain

--- sortingkotlin/src/String Manipulation.kt
+++ sortingkotlin/src/String Manipulation.kt
@@ -1,24 +1,23 @@
-fun main() {
-    val  makanan = setOf(" milkshake", " pizza", " chicken", " fries", " coke", " burger", " pizza", " sandwich", " milkshake", " pizza")
-    print(makanan.elementAt(5))
-    print(makanan.elementAt(3))
-    print(makanan.elementAt(2))
-    print(makanan.elementAt(1))
-    print(makanan.elementAt(1))
-    print(makanan.elementAt(1))
-    print(makanan.elementAt(6))
-    print(makanan.elementAt(0))
-    print(makanan.elementAt(0))
-    println(makanan.elementAt(4))
+fun stringManipulation(kataAsli: String, kataModifikasiBerurutan: List<String>): String {
 
+    var kataBenar = String()
 
-    val makanan2 = setOf(" pizza"," chicken"," fries"," burger"," coke"," milkshake"," fries"," sandwich")
-    print(makanan2.elementAt(3))
-    print(makanan2.elementAt(2))
-    print(makanan2.elementAt(2))
-    print(makanan2.elementAt(1))
-    print(makanan2.elementAt(0))
-    print(makanan2.elementAtOrNull(7))
-    print(makanan2.elementAt(6))
-    print(makanan2.elementAt(5))
+    for (value in kataModifikasiBerurutan) {
+        if (value.toLowerCase() in kataAsli) {
+            kataBenar += "$value "
+        }
+    }
+
+    return kataBenar;
 }
+
+fun main() {
+    var kata: String = "cokefriesburgerchickenpizzasandwichonionringsmilkshake"
+    var urutan: List<String> = listOf("Burger","Fries","Chicken",
+            "Pizza","Sandwich","Onionrings",
+            "Milkshake","Coke")
+    var result = stringManipulation(kata, urutan)
+
+    println(result)
+}
+
